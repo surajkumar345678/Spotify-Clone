086e92570aea6ea6fae90b157f1ca17972d30b2b
surajkumar345678 <kumarsuraj345678@gmail.com>
2024-02-10 12:57:06 +0530

    added script code for songlist

--- script.js
+++ script.js
@@ -9,18 +9,37 @@ async function getSongs() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
+            songs.push(element.href.split("/songs/")[1])
         }
-
     }
     return songs
 }
+
 async function main() {
     let songs = await getSongs()
-    console.log(songs)
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li>        
+                            <img class="invert" src="./images/music.svg" alt="">
+                            <div class="info">
+                                <div class="">${song.replaceAll("%20", " ")}</div>
+                                <div class="">Song Artist</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>    
+                                <img class="invert" src="./images/play.svg" alt="">
+                            </div>
+        </li>`;
+    }
 
     var audio = new Audio(songs[0]);
-    audio.play();
+    // audio.play();
 
+    audio.addEventListener("loadeddata", () => {
+        // let duration = audio.duration;
+        console.log(audio.duration, audio.currentSrc, audio.currentTime)
+    });
 }
+
 main()
